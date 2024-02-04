d508393e0ef8c91271b5e3afb19fdb2d4b5b1df8
Deivid182 <davidmendoza911119@gmail.com>
2024-02-04 13:49:30 -0600

    coverage intro

--- src/components/greet.spec.tsx
+++ src/components/greet.spec.tsx
@@ -1,14 +1,16 @@
 import { render, screen } from "@testing-library/react"
 import Greet from "./greet"
 
-test('Greet renders correctly', () => {
-  render(<Greet />)
-  const element = screen.getByText('Hello')
-  expect(element).toBeInTheDocument()
-})
-
-test('Greet renders correctly with name', () => {
-  render(<Greet name='John' />)
-  const element = screen.getByText('Hello John')
-  expect(element).toBeInTheDocument()
+describe('Greet', () => {
+  test('renders correctly', () => {
+    render(<Greet />)
+    const element = screen.getByText('Hello')
+    expect(element).toBeInTheDocument()
+  })
+  
+  test('renders a name', () => {
+    render(<Greet name='John' />)
+    const element = screen.getByText('Hello John')
+    expect(element).toBeInTheDocument()
+  })
 })
