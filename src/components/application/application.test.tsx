33d3eef84cf64fe3c25f39ca70b5027899742b7b
Deivid182 <davidmendoza911119@gmail.com>
2024-04-17 21:17:10 -0600

    aff test for the application component

---
+++ src/components/application/application.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import Application from "./application";
+
+describe("Application", () => {
+  test("renders correctly", () => {
+    render(<Application />);
+    const nameElement = screen.getByRole("textbox", { name: "Name" });
+    expect(nameElement).toBeInTheDocument();
+
+    const bioElement = screen.getByRole("textbox", { name: "Bio" });
+    expect(bioElement).toBeInTheDocument();
+
+    const jobLocationElement = screen.getByRole("combobox");
+    expect(jobLocationElement).toBeInTheDocument();
+
+    const termsElement = screen.getByRole("checkbox");
+    expect(termsElement).toBeInTheDocument();
+
+    const btnElement = screen.getByRole("button");
+    expect(btnElement).toBeInTheDocument();
+  });
+});
